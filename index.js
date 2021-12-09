71e40677a824e4db9d7d31dcbd2b942c40c8cc15
xiaoming wang <a345566462@163.com>
2021-12-09 13:14:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const UNICODE_MATCH_REG = /[^\x00-\xff]/g;
 const CONTENT_MATCH_REG = /(?<!-)content\s*:\s*([^;\}]+)/g;
 module.exports = function (source) {
-	source = source.replace(CONTENT_MATCH_REG, function (m, p1) {
+	return source.replace(CONTENT_MATCH_REG, function (m, p1) {
 		return m.replace(UNICODE_MATCH_REG, function (m) {
 			return "\\" + m.charCodeAt(0).toString(16);
 		});
 	});
-	return source;
 }
